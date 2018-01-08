31ebe1f0a72db3a5f2c414382f502f3e837e9cad
Omar Oskarsson <omaroskars15@gmail.com>
2018-01-08 23:26:28 +0000

    move file from  test data to tv shows

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # filesorter
 This is a file sorting application written in python that should sort files and folders for movies and tv shows based on Plex setup
 https://support.plex.tv/hc/en-us/categories/200028098-Media-Preparation
+
+# Learn Python
+https://automatetheboringstuff.com
+
+# Plex guide
+https://support.plex.tv/hc/en-us/categories/200028098-Media-Preparation
--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-import os
-import pprint
+import os, shutil, pprint
 
 WORKING_DIR = os.getcwd()
 PATHS = {
     'media_path': '',
     'tv_shows_path': '',
-    'movies_path': ''
+    'movies_path': '',
+    'data_path': os.path.join(WORKING_DIR, 'TestData')
 }
 
-def scan_tv_shows()
-    
+def move_file(source, destination):
+    shutil.copy(source, destination)
+
 def create_directory(key, path, name):
     print('Creating ' + name + ' under the path ' + os.path.join(path, name))
     if os.path.isdir(path + '\\' + name) == True:
-            print(name + " folder aldready exists")
+            print(name + " folder aldready exists\n")
             PATHS[key] = os.path.join(path, name)
     else:
         PATHS[key] = os.path.join(path, name)
         os.makedirs(PATHS[key])
-        print(name + ' created successfully')
-    
-    print()
+        print(name + ' created successfully\n')
+
+def init():
+    media_path = os.path.join(WORKING_DIR, 'Media')
+    if os.path.isdir(media_path) == True:
+        print('deleting all in ' + media_path)
+        shutil.rmtree(media_path)
+        print('Deleted Media folder successfully\n')
 
 def main():
+    init()
+
     create_directory('media_path', WORKING_DIR, 'Media')
     create_directory('tv_shows_path', PATHS['media_path'], 'TV shows')
     create_directory('movies_path', PATHS['media_path'], 'Movies')
 
     pprint.pprint(PATHS)
 
+    print(os.path.isfile(os.path.join(PATHS['data_path'], 'Peaky Blinders S01E01 Episode 1.avi')))
+    move_file(os.path.join(PATHS['data_path'], 'Peaky Blinders S01E01 Episode 1.avi'), PATHS['tv_shows_path'])
 
 main()
-
